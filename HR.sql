8e955b309afd064937114f00318a70b6a574bdab
junayetjun <junayet03@gmail.com>
2025-03-03 16:22:36 +0600

    03-03-25
    oracle data base

--- HR.sql
+++ HR.sql
@@ -94,10 +94,8 @@ select last_name, hire_date
 from employees
 where hire_date>'01-feb-05';
 
-
 select sysdate
 from dual;
-
 
 select current_timestamp
 from dual;
@@ -118,13 +116,10 @@ from employees
 where department_id in(20,50)
 order by last_name;
 
-
 --Q ---3
 select last_name, salary, department_id
 from employees 
 where salary BETWEEN 5000 and 12000 and department_id in(20,50);
-
-
 
 --Q ---4
 select last_name, salary, commission_pct
@@ -132,28 +127,19 @@ from employees
 where commission_pct is not null
 order by salary DESC , commission_pct desc;
 
-
-
 --Q ---5
 select last_name, salary, commission_pct
 from employees
 where commission_pct=0.20 ;
 
-
-
-
 --Q ---6
 select employee_id, last_name, salary, salary+round((salary*1.155)) as "New Salary"
 from employees;
-
-
 
 --02-03-25
 select last_name , salary ,
 to_char(salary*40,substr("BDT","$9,999,999.00"))as "Salary"
 from employees;
-
-
 
 SELECT last_name, salary,
 SUBSTR('BDT' || TO_CHAR(salary * 40, '9,999,999.00'), 1, 20) AS "Salary"
@@ -171,17 +157,64 @@ or last_name like 'A%'
 or last_name like 'M%'
 order by last_name;
 
-
 --Q--2
 select last_name, salary,
 lpad(salary, 15, '$') as "Salary"
 from employees;
 
-
 --Q--3
 select last_name, salary, salary+(salary*3) as "Dream Salaries"
 from employees;
 
+--03-03-25
+
+select last_name, job_id, salary,
+case job_id when 'IT_PROG' then 1.10*salary
+when 'ST_CLERK' THEN 1.15*salary
+when 'SA_REP' THEN 1.20*salary
+else salary  End "Revision Salary"
+from employees;
+
+select last_name, job_id, salary,
+(case when salary<5000 then 'Low'
+      when salary<10000 then 'Medium'
+      when salary<20000 then 'Good'
+      else 'Excellent'
+      end) "qualitive_Salary"
+      
+      from employees; 
+      
+      
+--      Class Test 3-03-25 ----
+
+-- Q--1
+SELECT FIRST_NAME, HIRE_DATE
+FROM EMPLOYEES
+WHERE HIRE_DATE BETWEEN '01-JAN-00' AND '31-DEC-05'
+ORDER BY 2;
+ 
+-- --Q--2
+ SELECT FIRST_NAME, HIRE_DATE, JOB_ID
+ FROM EMPLOYEES
+ WHERE JOB_ID LIKE '%IT_PROG%'
+ OR JOB_ID LIKE '%SA%'
+ ORDER BY 1;
+ 
+--Q--3
+SELECT FIRST_NAME, HIRE_DATE
+FROM EMPLOYEES
+WHERE HIRE_DATE > '01-JAN-08'
+ORDER BY 1 DESC;
+
+--Q--4
+SELECT *
+FROM EMPLOYEES
+WHERE EMPLOYEE_ID IN (150,160);
+
+--Q--5
+SELECT FIRST_NAME, SALARY, COMMISSION_PCT, HIRE_DATE
+FROM EMPLOYEES
+WHERE SALARY < 10000;
 
 
 
